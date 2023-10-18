9bb1b8ebe13450485b55af23038fc7e40b43a508
Bruno Meneguele <bmeneg@heredoc.io>
2023-10-18 16:24:04 -0300

    editorconfig: make sure to follow a simple coding style

--- 2022/1/rust/src/main.rs
+++ 2022/1/rust/src/main.rs
@@ -1,9 +1,5 @@
-use std::io::{
-    prelude::*,
-    self,
-    BufReader
-};
 use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
 
 #[derive(Clone, Copy)]
 struct Elf {
@@ -17,43 +13,46 @@ fn read_lines(fname: &str) -> io::Result<io::Lines<BufReader<File>>> {
 }
 
 trait RankedElfs {
-	fn ranked_insert(&mut self, value: Elf);
-	fn ranked_total(&self) -> i32;
+    fn ranked_insert(&mut self, value: Elf);
+    fn ranked_total(&self) -> i32;
 }
 
 impl RankedElfs for Vec<Elf> {
-	fn ranked_insert(&mut self, elf: Elf) {
-		let v_len = self.len();
+    fn ranked_insert(&mut self, elf: Elf) {
+        let v_len = self.len();
 
-		for i in 0..v_len {
-			if elf.calories > self[i].calories {
-				self.insert(i, elf);
-				self.pop();
-				return;
-			}
-		}
-	}
+        for i in 0..v_len {
+            if elf.calories > self[i].calories {
+                self.insert(i, elf);
+                self.pop();
+                return;
+            }
+        }
+    }
 
-	fn ranked_total(&self) -> i32 {
-		let mut total: i32 = 0;
-		self.iter().for_each(|elf| total += elf.calories);
-		return total;
-	}
+    fn ranked_total(&self) -> i32 {
+        let mut total: i32 = 0;
+        self.iter().for_each(|elf| total += elf.calories);
+        return total;
+    }
 }
 
 fn main() {
-	let mut elfs = vec![
-		Elf{id: 0, calories:0},
-		Elf{id: 0, calories:0},
-		Elf{id: 0, calories:0},
-	];
+    let mut elfs = vec![
+        Elf { id: 0, calories: 0 },
+        Elf { id: 0, calories: 0 },
+        Elf { id: 0, calories: 0 },
+    ];
     let mut sum: i32 = 0;
     let lines_iter = read_lines("input").expect("failed to open file");
 
     for (i, lines) in lines_iter.into_iter().enumerate() {
         if let Ok(line) = lines {
             if line.is_empty() {
-				elfs.ranked_insert(Elf{id: i, calories: sum});
+                elfs.ranked_insert(Elf {
+                    id: i,
+                    calories: sum,
+                });
                 sum = 0;
             } else {
                 sum += match line.parse::<i32>() {
@@ -64,9 +63,9 @@ fn main() {
         }
     }
 
-	// Debug only to generate the answers we need to for AOC
-	for elf in &elfs {
-		println!("{}: {}", elf.id, elf.calories);
-	}
-	println!("total: {}", elfs.ranked_total());
+    // Debug only to generate the answers we need to for AOC
+    for elf in &elfs {
+        println!("{}: {}", elf.id, elf.calories);
+    }
+    println!("total: {}", elfs.ranked_total());
 }
