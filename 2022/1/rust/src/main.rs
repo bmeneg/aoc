416a7a87bd874ded43eb9a30c909fb6446ffaed1
Bruno Meneguele <bmeneg@heredoc.io>
2023-10-18 16:21:42 -0300

    2022/1: complete the second part of first puzzle
    I decided to use a trait for creating a specific insert() and total()
    functions, thus we can practice a bit more of our Rustacean instead of
    jumping directly into the puzzle solution (we're not running against the
    clock).

--- 2022/1/rust/src/main.rs
+++ 2022/1/rust/src/main.rs
@@ -5,9 +5,9 @@ use std::io::{
 };
 use std::fs::File;
 
-#[derive(Debug)]
+#[derive(Clone, Copy)]
 struct Elf {
-    id: usize,
+    id: usize, // we don't really need id, that's just out of curiosity
     calories: i32,
 }
 
@@ -16,20 +16,44 @@ fn read_lines(fname: &str) -> io::Result<io::Lines<BufReader<File>>> {
     Ok(BufReader::new(file).lines())
 }
 
+trait RankedElfs {
+	fn ranked_insert(&mut self, value: Elf);
+	fn ranked_total(&self) -> i32;
+}
+
+impl RankedElfs for Vec<Elf> {
+	fn ranked_insert(&mut self, elf: Elf) {
+		let v_len = self.len();
+
+		for i in 0..v_len {
+			if elf.calories > self[i].calories {
+				self.insert(i, elf);
+				self.pop();
+				return;
+			}
+		}
+	}
+
+	fn ranked_total(&self) -> i32 {
+		let mut total: i32 = 0;
+		self.iter().for_each(|elf| total += elf.calories);
+		return total;
+	}
+}
+
 fn main() {
-    let mut elf_greater = Elf {id: 0, calories: 0};
+	let mut elfs = vec![
+		Elf{id: 0, calories:0},
+		Elf{id: 0, calories:0},
+		Elf{id: 0, calories:0},
+	];
     let mut sum: i32 = 0;
     let lines_iter = read_lines("input").expect("failed to open file");
 
     for (i, lines) in lines_iter.into_iter().enumerate() {
         if let Ok(line) = lines {
             if line.is_empty() {
-                if sum > elf_greater.calories {
-                    elf_greater = Elf{
-                        id: i,
-                        calories: sum
-                    }
-                }
+				elfs.ranked_insert(Elf{id: i, calories: sum});
                 sum = 0;
             } else {
                 sum += match line.parse::<i32>() {
@@ -40,5 +64,9 @@ fn main() {
         }
     }
 
-    println!("{}: {}", elf_greater.id+1, elf_greater.calories);
+	// Debug only to generate the answers we need to for AOC
+	for elf in &elfs {
+		println!("{}: {}", elf.id, elf.calories);
+	}
+	println!("total: {}", elfs.ranked_total());
 }
